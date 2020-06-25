57a368bb436e75d1f0a819f82338aaefcd913560
Jahkobe <Jpursell264@gmail.com>
2020-06-25 15:05:36 -0600

    matrix

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Character.create([
+
+	{ name: "Neo", description: "The chosen one" },
+	{ name: "Trinity", description: "I know why you're here, Neo. I know what you've been doing... why you hardly sleep, why you live alone, and why night after night, you sit by your computer." },
+	{ name: "Morpheus", description: "red pill or blue pill" },
+	{ name: "Agent Smith", description: "Mr. anderson" },
+
+])
